c4bfb62b6e516ac6356e503a9123501923c6734a
DOFLAMINGO <doffymingo519@gmail.com>
2025-05-14 16:03:08 +0000

    pssi2-deploy-front v1.0.0Tc1 - Correctif

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL  || 'http://localhost:5000/api' || 'https://backend-production-1db9.up.railway.app/api',
+  baseURL: import.meta.env.VITE_API_URL  || 'http://localhost:5000/api' || 'https://backend-production-2bfb.up.railway.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
